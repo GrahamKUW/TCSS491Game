7fd6ea6b2c4cd9252eee545634fb95ac47e828cb
SuperRedSwan <Kavongraham2015@gmail.com>
2026-01-25 16:13:23 -0800

    Testing Adding Multiple Static Objects

--- assets/scenes/scenedata/scene1data.js
+++ assets/scenes/scenedata/scene1data.js
@@ -5,7 +5,7 @@ const Scene1 = {
     {
       GameObject: {
         Name: "Test Object",
-        UniqueIdentifier: "Test Object",
+        UniqueIdentifier: "Test Object 1",
         Components: {
           Transform: {
             PositionX: 0,
@@ -20,6 +20,61 @@ const Scene1 = {
             CellSizeY: 32,
             Filter: false,
             IsFlipped: false,
+          },
+        },
+      },
+    },
+    {
+      GameObject: {
+        Name: "Test Object",
+        UniqueIdentifier: "Test Object 2",
+        Components: {
+          Transform: {
+            PositionX: 400,
+            ScaleX: 10,
+            ScaleY: 10,
+          },
+          Sprite: {
+            SpritesheetPath: "./robosprite.png",
+            CellSizeX: 32,
+            CellSizeY: 32,
+          },
+        },
+      },
+    },
+    {
+      GameObject: {
+        Name: "Test Object",
+        UniqueIdentifier: "Test Object 3",
+        Components: {
+          Transform: {
+            PositionY: 400,
+            ScaleX: 10,
+            ScaleY: 10,
+          },
+          Sprite: {
+            SpritesheetPath: "./robosprite.png",
+            CellSizeX: 32,
+            CellSizeY: 32,
+          },
+        },
+      },
+    },
+    {
+      GameObject: {
+        Name: "Test Object",
+        UniqueIdentifier: "Test Object 4",
+        Components: {
+          Transform: {
+            PositionX: 400,
+            PositionY: 400,
+            ScaleX: 10,
+            ScaleY: 10,
+          },
+          Sprite: {
+            SpritesheetPath: "./robosprite.png",
+            CellSizeX: 32,
+            CellSizeY: 32,
           },
         },
       },
