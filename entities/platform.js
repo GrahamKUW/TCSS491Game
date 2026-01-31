9eafca43d4df3ba7fd7649b01e9c6159649f6c8e
robbie2allen@gmail.com <robbieal@uw.edu>
2026-01-30 22:35:08 -0800

    Rats, Spikes, and Platforms working with new tilemap
    - Added Spikes, rat and platform back into our prototype level (added in main - positions can change)

--- entities/platform.js
+++ entities/platform.js
@@ -3,8 +3,8 @@ function createPlatform(x, y) {
     const entity = {
         removeFromWorld: false,
         position: new Position(x, y),
-        sprite: new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/platform.png"), 0, 0, 32, 16, 3 , 3),
-        collider: new Collider(96, 18), //x3 from sprite since scale is x3
+        sprite: new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/platform.png"), 0, 0, 32, 16, 2 , 2),
+        collider: new Collider(64, 12), //x2 from sprite since scale is x2
         static: new Static(),
     }
 
--- entities/spikes.js
+++ entities/spikes.js
@@ -3,8 +3,8 @@ function createSpike(x, y) {
     const entity = {
         removeFromWorld: false,
         position: new Position(x, y),
-        sprite: new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/spikes.png"), 0, 16, 32, 16, 3 , 3),
-        collider: new Collider(96, 48), //x3 from sprite since scale is x3
+        sprite: new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/spikes.png"), 0, 16, 32, 16, 2 , 2),
+        collider: new Collider(64, 32), //x2 from sprite since scale is x2
         static: new Static(),
         hazard: new Hazard()
     }
