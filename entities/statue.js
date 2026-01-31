ac3618adf3ab89d58e47b0310d80107fa14ec487
SuperRedSwan <kdg2023@uw.edu>
2026-01-30 21:03:23 -0800

    Final Fix

--- entities/statue.js
+++ entities/statue.js
@@ -7,7 +7,7 @@ function createStatue(x, y,) {
         position: new Position(x, y),
         velocity: new Velocity(0, 0),
         gravity: new Gravity(800),
-        collider: new Collider(32, 48, 14, 16),
+        collider: new Collider(32, 48, 14, 18),
         sprite: new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/StatueCatsMainCharacter-Sheet.png"), 0, 128, 32, 32, 2, 2)
     }
     return entity;
--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+// Just putting it here until we have a proper game data file or something
+const TILEMAP_POSITION_X = -400;
+const TILEMAP_POSITION_Y = -168;
+const TILEMAP_SCALE_X = 2;
+const TILEMAP_SCALE_Y = 2;
+const LEVEL_REFERENCE = "prototype_level";
+
 // QUEUE THE GAME MANIFEST
 ASSET_MANAGER.queueManifest(GameManifest.data);
 
@@ -23,20 +30,43 @@ ASSET_MANAGER.downloadAll(() => {
 	gameEngine.addSystem(new AnimationSystem());
 	gameEngine.addSystem(new RenderSystem());
 	gameEngine.addSystem(new EnemySystem());
-	gameEngine.addSystem(new DeathSystem());
-
-
+	//gameEngine.addSystem(new DeathSystem());
 
 	//these calls set up the prototype level, should be refactored so we can load different levels
-	gameEngine.addEntity(createBackground("./assets/sprites/prototype_background.png"));
-	gameEngine.addEntity(createPlayer(30, 656, 30, 600));
-	gameEngine.addEntity(createRat(600, 656, 50));
-	gameEngine.addEntity(createSpike(256, 656));
-	gameEngine.addEntity(createPlatform(770, 490));
+	
+	// Set up the tilemap background
+	
+	const background = createTilemapBackground("./assets/sprites/StatueCatsTileset.png",TILEMAP_POSITION_X,TILEMAP_POSITION_Y,  TILEMAP_SCALE_X, TILEMAP_SCALE_Y);
+	const palette = mapSpriteToTilePalette(background.sprite);
+	const tilemap = getTileMap(LEVEL_REFERENCE); 
+	background.sprite.tilemapData = {tilemap: tilemap, palette: palette};
+	gameEngine.addEntity(background);
+
+	gameEngine.addEntity(createPlayer(300, 425, 300, 425));
+	//gameEngine.addEntity(createRat(600, 656));
+	//gameEngine.addEntity(createSpike(256, 656));
+
+	// Create level collider
+	const colliderData = getTileMapColliders(LEVEL_REFERENCE);
+
+	for (let i = 0; i < colliderData.length; i++) {
+		const collider = colliderData[i]
+
+		const wallX = TILEMAP_POSITION_X + collider.x * TILEMAP_SCALE_X;
+		const wallY = TILEMAP_POSITION_Y + collider.y * TILEMAP_SCALE_Y;
+		const wallWidth = collider.width * TILEMAP_SCALE_X;
+		const wallHeight = collider.height * TILEMAP_SCALE_Y;
+
+		gameEngine.addEntity(createWall(wallX,wallY,wallWidth, wallHeight ));
+    
+  	}
+	/*
 	gameEngine.addEntity(createWall(0,704,1024,64));
 	gameEngine.addEntity(createWall(190,640,64,64));
 	gameEngine.addEntity(createWall(470,550,64, 154));
 	gameEngine.addEntity(createWall(780,640,64, 64));
+	*/
+	
 
 	gameEngine.start();
 });
