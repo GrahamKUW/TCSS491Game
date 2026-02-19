97928de9e513329f98938e4cc57692821520a40c
SuperRedSwan <kdg2023@uw.edu>
2026-02-19 09:44:10 -0800

    Added Level 1 and Level 2 Into the game
    Notes
    Gates need to be closed at start, for level 2 to work

--- entities/exitdoor.js
+++ entities/exitdoor.js
@@ -1,9 +1,10 @@
-function createExitDoor(x, y, id, width = 32, height = 20, offsetX = 0, offsetY = 12, targetLevel = "prototype_level") {
+function createExitDoor(x, y, id, width = 32, height = 20, offsetX = 0, offsetY = 12, scaleWidth = 1, scaleHeight = 1, targetLevel = "prototype_level") {
 
     const entity = {
         position: new Position(x,y),
         trigger: new Trigger(width, height, offsetX, offsetY, id),
         levelchange: new LevelChange(id, targetLevel),
+        sprite: new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/ExitEntrance.png"), 0, 0, 32, 32, scaleWidth , scaleHeight),
     }
 
     return entity;
