21464112fa0b3f99f6f81b1f77d5cc7ec65e034c
robbie2allen@gmail.com <robbieal@uw.edu>
2026-02-19 15:15:18 -0800

    Change Player speed

--- components/playercontrolled.js
+++ components/playercontrolled.js
@@ -1,6 +1,6 @@
 // PlayerControlled component - marks this entity as player-controlled
 class PlayerControlled {
-    constructor(speed = 100, respawnX = 0, respawnY = 0, isGrounded = false) {
+    constructor(speed = 50, respawnX = 0, respawnY = 0, isGrounded = false) {
         this.speed = speed;  // Movement speed in pixels per second
 
         this.respawnX = respawnX;
