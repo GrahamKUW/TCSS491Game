7926c2ef66281bea6dbda4aa0aaad424ec2c250d
SeanM <208197488+smille9@users.noreply.github.com>
2026-03-04 17:29:37 -0800

    Adding gate animations and adding an option for gates to begin closed or open as a parameter to the factory

--- entities/gate.js
+++ entities/gate.js
@@ -1,10 +1,10 @@
-function createGate(x, y, triggerID, colliderWidth = 16, colliderHeight = 64, colliderOffsetX = 8, colliderOffsetY) {
+function createGate(x, y, triggerID, colliderWidth = 16, colliderHeight = 64, colliderOffsetX = 8, colliderOffsetY, startClosed = true) {
 
     const activeSprite = new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/gate.png"), 48, 0, 16, 32, 2, 2);    //open
     const inactiveSprite = new Sprite(ASSET_MANAGER.getAsset("./assets/sprites/gate.png"), 0, 0, 16, 32, 2, 2);   //closed
 
     const doorAnimations = {
-        'active': {
+        'active': { //open
             frames: [
                 {x: 0, y:0, width: 16, height: 32},
                 {x: 16, y:0, width: 16, height: 32},
@@ -13,11 +13,23 @@ function createGate(x, y, triggerID, colliderWidth = 16, colliderHeight = 64, co
             ],
             duration: .15,
             loops: false
-        }, 'inactive': {
+        }, 'inactive': { //closed
             frames: [
                 {x: 48, y:0, width: 16, height: 32},
                 {x: 32, y:0, width: 16, height: 32},
                 {x: 16, y:0, width: 16, height: 32},
+                {x: 0, y:0, width: 16, height: 32},
+            ],
+            duration: .15,
+            loops: false
+        }, 'start-active': {
+            frames: [
+                {x: 48, y:0, width: 16, height: 32},
+            ],
+            duration: .15,
+            loops: false
+        }, 'start-inactive': {
+            frames: [
                 {x: 0, y:0, width: 16, height: 32},
             ],
             duration: .15,
@@ -26,18 +38,22 @@ function createGate(x, y, triggerID, colliderWidth = 16, colliderHeight = 64, co
     };
 
 
-    //This will have a collider added to it in ToggleCollider system.
-    const entity = { 
+    const entity = {
         removeFromWorld: false,
         position: new Position(x, y),
-        sprite: inactiveSprite,
+        sprite: startClosed ? inactiveSprite : activeSprite,
         static: new Static(),
-        togglecollider: new ToggleCollider(triggerID, colliderWidth, colliderHeight, colliderOffsetX, colliderOffsetY),
-       // togglesprite: new ToggleSprite(activeSprite, inactiveSprite, triggerID),
-        animator: new Animator(doorAnimations, 'active'),
-        toggleanimator: new ToggleAnimator(triggerID),
-
+        togglecollider: new ToggleCollider(triggerID, colliderWidth, colliderHeight, colliderOffsetX, colliderOffsetY, startClosed),
+        animator: new Animator(doorAnimations, startClosed ? 'start-inactive': 'start-active'),
+        toggleanimator: new ToggleAnimator(triggerID, !startClosed),
     }
 
+    //Doors that start open will have a collider added by the toggle collider.
+    if (startClosed) {
+        entity.collider = new Collider(colliderWidth, colliderHeight, colliderOffsetX, colliderOffsetY);
+    }
+    
+
     return entity;
+
 }
